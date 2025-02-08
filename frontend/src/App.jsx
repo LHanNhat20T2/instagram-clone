71e63b59157fb1775fd94fb46280de9305203269
HanNhat02 <hannhat2468@gmail.com>
2025-02-08 17:57:20 +0700

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import Login from "@/components/Login";
 import Signup from "@/components/Signup";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "@/components/Home";
-import MainLayout from "@/components/Mainlayout";
+import MainLayout from "@/components/MainLayout";
 import Profile from "@/components/Profile";
 import EditProfile from "@/components/EditProfile";
 import ChatPage from "@/components/ChatPage";
